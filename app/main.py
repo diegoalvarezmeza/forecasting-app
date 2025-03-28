496c89cd8a4622d463b8dd77ff400d3a3809a4d9
Diego Alvarez <diego.alvarez.meza.14@gmail.com>
2025-03-28 18:45:43 -0300

    feature/Adding-notebooks-EDA

---
+++ app/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="App de Pronóstico", layout="wide")
+st.title(" Time Series Forecasting App")
+
+st.write("""
+Esta aplicación permite realizar análisis exploratorio y pronóstico sobre datos transaccionales.
+Usa el menú lateral para explorar cada módulo.
+""")
